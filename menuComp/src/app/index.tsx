a2b0b19245529fac421310deef30db870b0f6eaf
laasya-villuri <laasyasv19@gmail.com>
2026-09-17 10:08:48 -0500

    need to style and make it pretty and make sense

--- menuComp/src/app/index.tsx
+++ menuComp/src/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
     <View style={styles.container}>
       <MyComponent />
       <Text style={styles.titleText}>Mobile Apps</Text>
-      <Text style={styles.bodyText}>Alex</Text>
+      <Text style={styles.bodyText}>Alex and Laasya</Text>
     </View>
   );
 }
