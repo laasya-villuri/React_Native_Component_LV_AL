228aa78dcf513773e3c213d3a91768f514a498b4
laasya-villuri <laasyasv19@gmail.com>
2026-09-17 09:34:23 -0500

    Update index.tsx

--- menuComp/src/app/index.tsx
+++ menuComp/src/app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Laasya</Text>
     </View>
   );
 }
