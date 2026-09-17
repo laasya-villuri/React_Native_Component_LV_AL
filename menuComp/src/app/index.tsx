a8d96d40cfdeed670a0740e72718d5cca2f4022e
M1d4y <lim81617@gmail.com>
2026-09-17 10:02:08 -0500

    Update index.tsx

--- menuComp/src/app/index.tsx
+++ menuComp/src/app/index.tsx
@@ -1,32 +1,36 @@
-import * as React from 'react';
-import { View } from 'react-native';
-import { Button, Menu, Divider, PaperProvider } from 'react-native-paper';
+import { StyleSheet, Text, View } from "react-native";
+//this is where we import components that are provided
+//by RN. Notice they start wit ha capital letter
+// Forgetting to import is a super common error
 
-const MyComponent = () => {
-  const [visible, setVisible] = React.useState(false);
-
-  const openMenu = () => setVisible(true);
-
-  const closeMenu = () => setVisible(false);
-
+export default function Index() {
   return (
-    <PaperProvider>
-      <View
-        style={{
-          paddingTop: 50,
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}>
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={<Button onPress={openMenu}>Show menu</Button>}>
-          <Menu.Item onPress={() => {}} title="Item 1" />
-          <Menu.Item onPress={() => {}} title="Item 2" />
-          <Divider />
-          <Menu.Item onPress={() => {}} title="Item 3" />
-        </Menu>
-      </View>
-    </PaperProvider>
+    <View style={styles.container}>
+      <Menu theme={{ colors: { primary: "green" } }} />
+      <Text style={styles.titleText}>Mobile Apps</Text>
+      <Text style={styles.bodyText}>Alex</Text>
+    </View>
   );
-};
+}
+
+// rnss
+// within styles I will define any styles I want for this page
+//Later we will learn how to define styles externally so they
+//can be used across many files
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 28, // notice no '' around numerical values
+    color: "purple", //there are '' around string values,
+    // or predefined options
+    fontWeight: "bold",
+  },
+  bodyText: {
+    fontSize: 18,
+    color: "#338855",
+  },
+});
